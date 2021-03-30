cc889750235787ae0874c7c191a5a10393411262
Francesca Marino <fjmarino90@gmail.com>
2021-03-30 16:18:40 +0900

    added login component and style files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
 import "../styles/App.css";
+import Login from "./Login";
 import Navbar from "./Navbar";
 import MapContainer from "./MapContainer";
 import BattleContainer from "./BattleContainer";
@@ -7,6 +8,7 @@ import StatsContainer from "./StatsContainer";
 function App() {
   return (
     <div className="App">
+      <Login />
       <Navbar />
       <MapContainer />
       <BattleContainer />
