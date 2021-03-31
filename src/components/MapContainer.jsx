869bb2ff5f07a9df6285334659e3eb42b33d4372
BBennettChiba <BrysonCBennett@gmail.com>
2021-03-31 09:16:03 +0900

    brysons work from yesterday

--- src/components/MapContainer.jsx
+++ src/components/MapContainer.jsx
@@ -1,7 +1,26 @@
 import "../styles/MapContainer.css";
+import axios from "axios";
+axios.defaults.headers.common["Authorization"] = process.env.REACT_APP_API_KEY;
+axios.defaults.headers.get["Access-Control-Allow-Origin"] = "*";
+
+
 
 function MapContainer() {
-  return <section className="MapContainer">MapContainer </section>;
+  async function getPins() {
+    try {
+      const pins = await axios.get(
+        "http://localhost:4000/user/Bryson");
+      console.log(pins);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  return (
+    <section className="MapContainer" onClick={getPins}>
+      MapContainer{" "}
+    </section>
+  );
 }
 
 export default MapContainer;
