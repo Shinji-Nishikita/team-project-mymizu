a4592a2ec2560dea0980bb5540eabbeb1135689b
BBennettChiba <BrysonCBennett@gmail.com>
2021-03-31 16:32:03 +0900

    added button components

--- src/components/AttackContainer.jsx
+++ src/components/AttackContainer.jsx
@@ -1,10 +1,13 @@
 import "../styles/AttackContainer.css";
-
+import Button from './Button'
 function AttackContainer() {
   const changeColor = () => { }
   return <section className="AttackContainer">
-    <section>
-      <button
+    {/* <section> */}
+      <Button AttackAmount={350}/>
+      <Button AttackAmount={500}/>
+      <Button AttackAmount={750}/>
+      {/* <button
         style={{ color: "black" }}
         onClick={(e) => {
           console.log("e.target.color", e.target)
@@ -12,11 +15,11 @@ function AttackContainer() {
         }}
       >350</button>
       <button>500</button>
-      {/* <button>other</button> */}
+      <button>other</button>
     </section>
     <button
-      onClick={() => { }}>Attack</button>
-  </section>;
+      onClick={() => { }}>Attack</button> */}
+  </section>
 }
 
 export default AttackContainer;
