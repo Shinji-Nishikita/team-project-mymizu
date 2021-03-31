d758c93d575c287b9bcc0b6a8ea403e3c9465424
BBennettChiba <BrysonCBennett@gmail.com>
2021-03-31 16:20:27 +0900

    changed something in stats container

--- src/components/StatsContainer.jsx
+++ src/components/StatsContainer.jsx
@@ -1,9 +1,21 @@
-import React from "react";
+import {useEffect} from "react";
 import "../styles/StatsContainer.css";
 
-function StatsContainer(props) {
-  
-  return <section className="StatsContainer">Stats container </section>;
+function StatsContainer({userData}) {
+
+  useEffect(()=>{
+    console.log(userData)
+  }, [])
+
+  return <section className="StatsContainer">
+  <form>
+    <div><b>User: </b>{userData.name}</div>
+    <div><b>Username: </b>{userData.username}</div>
+    <div><b>Level: </b>{userData.level}</div>
+    <div><b>Total refills: </b>{userData.refill_count}</div>
+    <div><b>Refills amount: </b>{userData.refill_amount}</div>
+  </form>
+  </section>;
 }
 
 export default StatsContainer;
