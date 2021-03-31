8cf4cbac797023041027992e6d4954b061d21e27
BBennettChiba <BrysonCBennett@gmail.com>
2021-03-31 09:49:38 +0900

    added correct api call

--- src/components/MapContainer.jsx
+++ src/components/MapContainer.jsx
@@ -2,15 +2,17 @@ import React from "react";
 import "../styles/MapContainer.css";
 import axios from "axios";
 axios.defaults.headers.common["Authorization"] = process.env.REACT_APP_API_KEY;
-axios.defaults.headers.get["Access-Control-Allow-Origin"] = "*";
+// axios.defaults.headers.get["Access-Control-Allow-Origin"] = "*";
 
 
 
 function MapContainer() {
   async function getPins() {
+    const user = await axios.get("http://localhost:4000/user/Bryson")
+    console.log(user.data);
     try {
       const pins = await axios.get(
-        "http://localhost:4000/user/Bryson");
+        "https://my-mizu-dev2-gen8n.ondigitalocean.app/dev-api/search/radius?longitude=139.73655447363853&latitude=35.671812626599866&radius=500");
       console.log(pins);
     } catch (err) {
       console.log(err);
